ea4b35dba3ba73be30443773bbfb07c09aa56a62
ElvinYekayev <elvinyeka@gmail.com>
2023-09-28 18:45:52 +0400

    eslint prettier config added

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Test from 'components/Test'
+
+function App() {
+  return (
+    <div>
+      <h1>init project</h1>
+      <Test />
+    </div>
+  )
+}
+
+export default App
